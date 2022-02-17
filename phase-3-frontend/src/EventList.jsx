4caa73c52ecf25dff8a037f9dc0c8af37f4000ec
shani <shhhaniburde@gmail.com>
2022-02-17 17:41:49 -0500

    Added next functionality

--- phase-3-frontend/src/EventList.jsx
+++ phase-3-frontend/src/EventList.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import EventCard from './EventCard'
 
 function EventList({data, handleDeleteEvent, handleUpdateEvent}) {
-    const eventList = data.map((data) => 
+    
+  const [dataIndex, setDataIndex] = useState(0)
+
+  
+  const eventList = data
+    .slice(dataIndex, dataIndex + 8)
+    .map((data) => 
     <EventCard
         key={data.id}
         data={data}
         handleDeleteEvent={handleDeleteEvent}
         handleUpdateEvent={handleUpdateEvent}
     />)
+
+    function handleClickMore() {
+      setDataIndex((dataIndex) => (dataIndex + 8) % data.length);
+    }
+
   return (
+    <>
     <div className="event-list">
     {eventList}
     </div>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <div className="next-container">
+        <button className="next-button" onClick={handleClickMore}>Next</button>
+    </div>
+    </>    
   )
 }
 
