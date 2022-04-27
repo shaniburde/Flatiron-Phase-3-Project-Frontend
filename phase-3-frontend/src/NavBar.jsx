4ec0e558828dc34cc27cc48c91ea2f10beeb854e
Shani <shhhaniburde@gmail.com>
2022-04-27 11:14:03 -0400

    added script start

--- phase-3-frontend/src/NavBar.jsx
+++ phase-3-frontend/src/NavBar.jsx
@@ -5,10 +5,10 @@ import { NavLink } from 'react-router-dom';
 export default function NavBar() {
   return (
     <div>
-        <NavLink exact to="/my-events" className="nav-button"> My Events </NavLink>
-        <NavLink exact to="/create-event" className="nav-button"> New Event </NavLink>
-        <NavLink exact to="/create-venue" className="nav-button"> New Venue </NavLink>
-        <NavLink to='/stats' className="nav-button">Artist Stats</NavLink>
+        <NavLink exact to="/" className="nav-button"> My Events </NavLink>
+        <NavLink exact to="/" className="nav-button"> New Event </NavLink>
+        <NavLink exact to="/" className="nav-button"> New Venue </NavLink>
+        <NavLink to='/' className="nav-button">Artist Stats</NavLink>
     </div>
   )
 }
